d34d2b92a621770630317ccdfefa4129b953b36b
a.gridnev <anton@gridnev.pro>
2022-12-26 16:07:17 +1100

    Fix settings

--- settings_example.py
+++ settings_example.py
@@ -1,4 +1,6 @@
-TM_TOKEN = "5921930125:AAFMxV1F-VNj4gwvine73N_bz-4TDYg3mGM"
-PROXY_URL = "http://10.0.0.9:8080"
-URL = "https://autosakhcom.ru/sales/auto?f%5Bcategory_id%5D=1&is_extended=1&f%5Bprice_e%5D=300000&f%5Bto_order%5D=0&sort=newest#search-description"
-TIMER = 10
+TM_TOKEN = ""
+PROXY_SWITCH = False
+PROXY_URL = ""
+DOMAIN = "https://autosakhalin.ru"
+URL = "https://autosakhalin.ru/sales/auto?f%5Bcategory_id%5D=1&f%5Bbrand_id%5D=30&f%5Byear_s%5D=2007&f%5Bprice_e%5D=550000&f%5Bto_order%5D=0"
+TIMER = 60
